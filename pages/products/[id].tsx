c31689795ebd912341bc9bc3805c2353f60bc605
Juan Villacorta <jvillacorta@norkut.com.ve>
2021-11-29 00:29:39 -0400

    ...

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -9,6 +9,8 @@ import { formatDate, printElement } from '@/lib/utils/client'
 import { Button } from '@/components/button'
 import { Printer24 } from '@carbon/icons-react'
 import { Chart } from '@/components/chart'
+import Image from 'next/image'
+import Logo from '@/public/venita.png'
 
 type Reports = {
   product: Product
@@ -58,8 +60,8 @@ const ProductReports: PageWithLayout = () => {
   return (
     <div className="py-4 c-lg">
       <div className="flex text-xs w-full pb-6 uppercase">
-        <Link href="/products" className="hover:underline px-3 py-2 bg-white border border-gray-100 shadow flex items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <Link href="/products" className="bg-bg-secondary border flex border-gray-100 shadow py-2 px-3 items-center hover:underline">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 mr-2 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
           </svg>
         </Link>
@@ -73,14 +75,17 @@ const ProductReports: PageWithLayout = () => {
         <div ref={wrapperRef}>
           <Viewport className="w-full animate" once style={setAnim({ y: '-0.3rem' })}>
             <div className="flex flex-col space-y-6">
+              <div className="mx-auto hidden print:flex">
+                <Image src={Logo} width={190} height={94} objectFit="contain" loading="eager" />
+              </div>
               <div className="flex flex-col space-y-2">
-                <div className="flex items-center sm:mb-0 bg-white px-3 py-2 shadow">
-                  <h2 className="font-bold leading-normal text-xl text-gray-700">
+                <div className="bg-bg-secondary rounded-lg flex shadow p-4 items-center print:shadow-none print:p-0 sm:mb-0">
+                  <h2 className="font-bold text-xl text-fg-secondary leading-normal">
                     {data.product.name}
                   </h2>
                 </div>
 
-                <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 items-end lg:grid-cols-5 bg-white px-3 py-2 shadow">
+                <div className="bg-bg-secondary rounded-lg shadow grid p-4 gap-8 grid-cols-1 items-end print:shadow-none print:p-0 sm:grid-cols-2 lg:grid-cols-5">
                   <fieldset className="flex flex-col w-full animate" style={setAnim({ d: '100ms' })}>
                     <label htmlFor="init" className="input-label">Fecha de inicio</label>
                     <input
@@ -109,7 +114,7 @@ const ProductReports: PageWithLayout = () => {
                     />
                   </fieldset>
 
-                  <Button title="Filtrar" className="justify-center items-center" onClick={filter} />
+                  <Button title="Filtrar" className="justify-center items-center print:hidden" onClick={filter} />
                 </div>
               </div>
 
@@ -120,7 +125,7 @@ const ProductReports: PageWithLayout = () => {
                 </div>
                 <div className="flex flex-col space-y-2 text-right w-full card">
                   <h4 className="font-bold text-lg">Ganancias totales:</h4>
-                  <p className="font-bold text-green-500 text-4xl">${data.sellDetails.map(p => p.price * p.quantity).reduce((a, b) => a + b, 0).toFixed(2)}</p>
+                  <p className="font-bold text-4xl">${data.sellDetails.map(p => p.price * p.quantity).reduce((a, b) => a + b, 0).toFixed(2)}</p>
                 </div>
                 {/* <div className="flex flex-col space-y-2 text-right w-full card">
                   <h4 className="font-bold text-lg">Unidades solicitadas:</h4>
@@ -128,11 +133,11 @@ const ProductReports: PageWithLayout = () => {
                 </div> */}
                 <div className="flex flex-col space-y-2 text-right w-full card">
                   <h4 className="font-bold text-lg">Inversión total:</h4>
-                  <p className="font-bold text-red-500 text-4xl">${data.buyDetails.map(p => p.price * p.quantity).reduce((a, b) => a + b, 0).toFixed(2)}</p>
+                  <p className="font-bold text-4xl">${data.buyDetails.map(p => p.price * p.quantity).reduce((a, b) => a + b, 0).toFixed(2)}</p>
                 </div>
                 <div className="flex flex-col space-y-2 text-right w-full card">
                   <h4 className="font-bold text-lg">Estadísticas</h4>
-                  <p className="font-bold">Pedido óptimo: {data.product.max - Math.max(0, data.product.stock)} unidades</p>
+                  <p className="font-bold">Pedido óptimo: {Math.max(0, data.product.max - Math.max(0, data.product.stock))} unidades</p>
                   <p className="font-bold">Punto de reorden: {data.product.min} unidades</p>
                   <p className="font-bold">C. Amortiguadora: {data.product.max - data.product.min} unidades</p>
                 </div>
@@ -145,7 +150,7 @@ const ProductReports: PageWithLayout = () => {
                   </div>
                 ) : null}
               </div>
-              <div className="flex mx-auto w-full pb-16 justify-end lg:w-9/10">
+              <div className="flex mx-auto w-full pb-16 justify-end">
                 <Button className="print:hidden" onClick={() => printElement(wrapperRef.current)} icon={<Printer24 />}>Exportar reportes</Button>
               </div>
             </div>
